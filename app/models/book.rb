3d0e7af6b1ce54b1be5540fdda58eec8a9e113fe
Tatsuhi Nii <nichenri1995@gmail.com>
2015-09-29 16:32:11 +0900

    model修正+booksまわり微調整

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,10 +14,25 @@ class Book < ActiveRecord::Base
   has_many :reservation_users, through: :book_reservations, :source => 'user'
 
 
+
+  def borrowing_records
+    BorrowingHistory.where(stock_id: self.stocks).order("created_at DESC")
+  end
+
+  def borrowing_stocks
+    Borrowing.where(stock_id: self.stocks)
+  end
+
+  def reserved_stocks
+    StockReservation.where(stock_id: self.stocks)
+  end
+
+
   def remaining_stocks
-   borrowing_stock_ids = Borrowing.where(stock_id: self.stocks).pluck(:stock_id)
-   reserved_stock_ids = StockReservation.where(stock_id: self.stocks).pluck(:stock_id)
-   self.stocks.where.not(id: borrowing_stock_ids + reserved_stock_ids)
+   borrowing_stock_ids = self.borrowing_stocks.pluck(:stock_id)
+   reserved_stock_ids = self.reserved_stocks.pluck(:stock_id)
+   remaining_stock_ids = self.stock_ids - borrowing_stock_ids - reserved_stock_ids
+   stocks.where(id: remaining_stock_ids)
   end
 
 
