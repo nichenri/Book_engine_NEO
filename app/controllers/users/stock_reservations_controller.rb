7bf054b5d571d7701e1850b255a8a9b206c8757d
Tatsuhi Nii <nichenri1995@gmail.com>
2015-10-03 12:42:33 +0900

    stock_reserv削除に伴うバグの修正

--- app/controllers/users/stock_reservations_controller.rb
+++ app/controllers/users/stock_reservations_controller.rb
@@ -9,15 +9,24 @@ class Users::StockReservationsController < UsersController
 
   def destroy
     @book = Book.find(params[:book_id])
-    StockReservation.find(params[:id]).destroy
+    ActiveRecord::Base.transaction do
+      StockReservation.find(params[:id]).destroy!
+      reservation = @book.book_reservations.all.order("created_at").first
+      if reservation
+        StockReservation.create!(user_id: reservation.user_id, stock_id: params[:stock_id], invalid_at: DateTime.now + 3.days)
+      end
+    end
     redirect_to users_book_path(@book.id)
+  rescue => e
+    p e
+    redirect_to :back
   end
 
 
   private
 
-    def stock_reservation_params
-      params.permit(:stock_id).merge(user_id: current_user.id, invalid_at: DateTime.now + 3.days)
-    end
+  def stock_reservation_params
+    params.permit(:stock_id).merge(user_id: current_user.id, invalid_at: DateTime.now + 3.days)
+  end
 
 end
