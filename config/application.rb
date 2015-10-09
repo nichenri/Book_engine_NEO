d12e46abf80c77f550d1fb9b449f613b30551d3a
Tatsuhi Nii <nichenri1995@gmail.com>
2015-10-09 11:32:08 +0900

    Rspecセットアップ完了

--- config/application.rb
+++ config/application.rb
@@ -25,4 +25,16 @@ module BooksEngine
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
+
+
+  config.generators do |g|
+    g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: true,
+      request_specs: false
+    g.fixture_replacement :factory_girl, dir: "spec/factories"
+  end
 end
